042931358604b1d72e83d8d945495b2e7b418f63
weibin <1292393108@qq.com>
2019-07-09 17:10:14 +0800

    update

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.set('view engine', 'ejs')// 设置模板引擎为 ejs
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 
-// 错误处理
 //错误处理
 app.use(function (err, req, res, next) {
   console.error(err.stack)
