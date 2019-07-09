84ae7f76724e4572d4e964b0e0804cbd3686c93b
weibin <1292393108@qq.com>
2019-07-09 18:38:15 +0800

    update

--- middlewares/check.js
+++ middlewares/check.js
@@ -1,12 +1,13 @@
 module.exports = {
-  checkLogin (req, res, next) {
-    if(!req.session.user) {
+  checkLogin: function checkLogin (req, res, next) {
+    if (!req.session.user) {
       req.flash('error', '未登录')
       return res.redirect('/signin')
     }
     next()
   },
-  checkNotLogin(req, res, next) {
+
+  checkNotLogin: function checkNotLogin (req, res, next) {
     if (req.session.user) {
       req.flash('error', '已登录')
       return res.redirect('back')// 返回之前的页面
