1906d3b55658a1881d5629fa252c06024df13cfa
weibin <1292393108@qq.com>
2019-07-09 16:42:04 +0800

    handleError

--- index.js
+++ index.js
@@ -11,4 +11,11 @@ app.set('view engine', 'ejs')// 设置模板引擎为 ejs
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 
+// 错误处理
+//错误处理
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.listen(3002)
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   res.send('hello, express')
+  // next(new Error('haha'))
 })
 
 module.exports = router
