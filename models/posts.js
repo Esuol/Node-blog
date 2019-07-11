1692e7c8b72d97d2c1f5bf0de43c1bc7ba8a048c
weibin <1292393108@qq.com>
2019-07-11 15:48:23 +0800

    pstPlugin

--- models/posts.js
+++ models/posts.js
@@ -1,4 +1,21 @@
+const marked = require('marked')
 const Post = require('../lib/mongo').Post
+
+// 将 post 的 content 从 markdown 转换成 html
+Post.plugin('contentToHtml', {
+  afterFind(posts) {
+    return posts.map(post => {
+      post.content = marked(post.content)
+      return post
+    })
+  },
+  afterFindOne: function (post) {
+    if (post) {
+      post.content = marked(post.content)
+    }
+    return post
+  }
+})
 
 module.exports = {
   // 创建一篇文章
