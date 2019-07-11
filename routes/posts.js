27be08f749aa9256d4dd595084afe0d8c3a70142
weibin <1292393108@qq.com>
2019-07-11 15:38:35 +0800

    发表文章

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const router = express.Router()
 
+const PostModel = require('../models/posts')
 const checkLogin = require('../middlewares/check').checkLogin
 
 // GET /posts 所有用户或者特定用户的文章页
@@ -12,14 +13,47 @@ router.get('/', (req, res, next) => {
   res.render('posts')
 })
 
-// POST /posts/create 发表一篇文章
-router.post('/create', checkLogin,  (req, res, next) => {
-  res.send('发表文章')
-})
-
 // GET /posts/create 发表文章页
 router.get('/create', checkLogin,  (req, res, next) => {
   res.render('create')
+})
+
+// POST /posts/create 发表一篇文章
+router.post('/create', checkLogin,  (req, res, next) => {
+// 获取参数
+ const author = req.session.user._id
+ const title = req.fields.title
+ const content = req.fields.content
+
+ //校验参数
+ try {
+  if(!title.length) {
+    throw new Error('请填写标题')
+  }
+  if(!content.length) {
+    throw new Error('请填写内容')
+  }
+ } catch (e) {
+   req.flash('error', e.message)
+   return res.redirect('back')
+ }
+
+ let post = {
+   author,
+   title,
+   content
+ }
+
+ PostModel.create(post)
+  .then(result => {
+    // 此 post 是插入 mongodb 后的值，包含 _id
+    post = result.ops[0]
+    req.flash('success', '插入成功')
+    // 发表成功后跳转到文章列表页
+    res.redirect(`/posts/${post._id}`)
+  })
+  .catch(next)
+
 })
 
 // GET /posts/:postId 单独一篇的文章页
