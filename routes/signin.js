ee551221cb8cdf754abf9ffaf0ac5ffb5e88af8a
weibin <1292393108@qq.com>
2019-07-11 15:08:58 +0800

    登陆

--- routes/signin.js
+++ routes/signin.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
+// sha1加密
+const sha1 = require('sha1')
 const express = require('express')
 const router = express.Router()
 
+const UserModel = require('../models/users')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 // GET /signin 登录页
@@ -11,7 +14,44 @@ router.get('/', checkNotLogin, function (req, res, next) {
 
 // POST /signin 用户登录
 router.post('/', checkNotLogin, function (req, res, next) {
-  res.send('登录')
+  const name = req.fields.name
+  let password = req.fields.password
+
+  // 校验参数
+  try {
+    if(!name.length) {
+      throw new Error('请填写用户名')
+    }
+    if (!password.length) {
+      throw new Error('请填写密码')
+    }
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  UserModel.getUserByName(name)
+    .then(user => {
+      if(!user) {
+        req.flash('error', '用户不存在')
+        return res.redirect('back')
+      }
+      // 检查密码是否匹配
+      if(sha1(password) !== user.password) {
+        req.flash('error', '用户名或密码错误')
+        return res.redirect('back')
+      }
+      // 登陆成功
+      req.flash('success', '登陆成功')
+      // 用户信息写入session
+      delete user.password
+      req.session.user = user
+       // 跳转到主页
+       res.redirect('/posts')
+    })
+    .catch(next)
 })
+
+
 
 module.exports = router
