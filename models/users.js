19e7903884e0b34908eabec179b106c687495131
weibin <1292393108@qq.com>
2019-07-11 14:14:13 +0800

    signup

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const User = require('../lib/mongo').User
 
 module.exports = {
   // 注册一个用户
-  create: function create (user) {
+  create (user) {
     return User.create(user).exec()
   }
 }
--- routes/signup.js
+++ routes/signup.js
@@ -13,12 +13,13 @@ router.get('/', checkNotLogin, function (req, res, next) {
   res.render('signup')
 })
 
-// POST /signup 用户注册
-router.post('/', checkNotLogin, function (req, res, next) {
+// POST/ signup 用户注册
+
+router.post('/', checkNotLogin, (req, res, next) => {
   const name = req.fields.name
   const gender = req.fields.gender
   const bio = req.fields.bio
-  const avatar = req.files.avatar.path.split(path.sep).pop()
+  const avatar = req.fields.avatar.path.split(path.sep).pop()
   let password = req.fields.password
   const repassword = req.fields.repassword
 
@@ -43,45 +44,44 @@ router.post('/', checkNotLogin, function (req, res, next) {
       throw new Error('两次输入密码不一致')
     }
   } catch (e) {
-    // 注册失败，异步删除上传的头像
-    fs.unlink(req.files.avatar.path)
-    req.flash('error', e.message)
-    return res.redirect('/signup')
+     // 注册失败，异步删除上传的头像
+     fs.unlink(req.files.avatar.path)
+     req.flash('error', e.message)
+     return res.redirect('/signup')
   }
-
-  // 明文密码加密
+  // 明文加密
   password = sha1(password)
 
   // 待写入数据库的用户信息
   let user = {
-    name: name,
-    password: password,
-    gender: gender,
-    bio: bio,
-    avatar: avatar
+    name,
+    password,
+    gender,
+    bio,
+    avatar
   }
   // 用户信息写入数据库
   UserModel.create(user)
-    .then(function (result) {
+    .then(result => {
       // 此 user 是插入 mongodb 后的值，包含 _id
       user = result.ops[0]
-      // 删除密码这种敏感信息，将用户信息存入 session
-      delete user.password
-      req.session.user = user
-      // 写入 flash
-      req.flash('success', '注册成功')
-      // 跳转到首页
-      res.redirect('/posts')
+     // 删除密码这种敏感信息，将用户信息存入 session
+     delete user.password
+     req.session.user = user
+     // 写入 flash
+     req.flash('success', '注册成功')
+     // 跳转首页
+     res.redirect('/posts')
     })
-    .catch(function (e) {
-      // 注册失败，异步删除上传的头像
+    .catch (e => {
+      // 注册失败， 异步删除上传头像
       fs.unlink(req.files.avatar.path)
       // 用户名被占用则跳回注册页，而不是错误页
-      if (e.message.match('duplicate key')) {
-        req.flash('error', '用户名已被占用')
-        return res.redirect('/signup')
-      }
-      next(e)
+     if (e.message.match('duplicate key')) {
+      req.flash('error', '用户名已被占用')
+      return res.redirect('/signup')
+     }
+     next(e)
     })
 })
 
