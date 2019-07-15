49860387634f1977aab5e943a63ecb362f07278e
weibin <1292393108@qq.com>
2019-07-15 12:50:44 +0800

    删除新增留言

--- models/comments.js
+++ models/comments.js
@@ -13,4 +13,19 @@ Comment.plugin('contentToHtml', {
 
 module.exports = {
   // 创建一个留言
+  create (comment) {
+    return Comment.create(comment).exec()
+  },
+  // 通过留言 id 获取一个留言
+  getCommentById (commentId) {
+    return Comment.findOne({_id: commentId}).exec()
+  },
+  // 通过留言 id 删除一个留言
+  deleteCommentById (commentId) {
+    return Comment.deleteOne({_id: commentId}).exec()
+  },
+   // 通过文章 id 删除该文章下所有留言
+  delCommentsByPostId (postId) {
+    return Comment.deleteMany({postId}).exec()
+  }
 }
